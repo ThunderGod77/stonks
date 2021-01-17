0041a1ef7421ca7566477e0d32d195844f40d6cb
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-01-17 17:33:38 +0530

    exporting Notes from db.js

--- database/db.js
+++ database/db.js
@@ -6,6 +6,6 @@ MongoClient.connect(uri).then((db) => {
   console.log("i connected successfully");
   exports.Users = db.db("hackathon").collection("users");
   exports.Courses =  db.db("hackathon").collection("courses");
-  exports.userCol = db.db("hackathon").collection("notes");
+  exports.Notes = db.db("hackathon").collection("notes");
 
 }).catch(console.dir);
